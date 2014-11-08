ef7446637ac237c6c8e284597e130cf39e24d5b2
Eli Pacheco Hoyos <eph074@gmail.com>
2014-11-08 00:03:00 -0500

    Deleting some files generated by the IDE

---
+++ Floristeria/app/views/emails/register.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <head></head>
+    <body>
+        <h1>{{$user}} bienvenido a la aplicacion Floristeria</h1>
+        <p>Confirme su registro a través del siguiente enlace: </p>
+        {{$msg}}
+    </body>
+</html>
